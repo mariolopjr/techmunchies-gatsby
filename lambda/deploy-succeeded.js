78b84911b3e97d4d8231be0a1856f93037c96589
Mario <mario@techmunchies.net>
2019-06-09 15:21:42 -0400

    Added Netlify functions to clear Cloudflare cache on successful build

---
+++ lambda/deploy-succeeded.js
@@ -0,0 +1,26 @@
+import fetch from "node-fetch"
+
+const { CLOUDFLARE_ZONE_ID, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY } = process.env
+
+exports.handler = async () => {
+  const apiUrl = `https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache`
+
+  // Purge Cloudflare
+  fetch(apiUrl, {
+    headers: {
+      "X-Auth-Email": CLOUDFLARE_EMAIL,
+      "X-Auth-Key": CLOUDFLARE_AUTH_KEY,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ "purge_everything": true })
+  })
+    .then(() => ({
+      statusCode: 200,
+      body: `Successfully purged Cloudflare cache!`,
+    }))
+    .catch(error => ({
+      statusCode: 422,
+      body: `Oops! Something went wrong. ${error}`,
+    }))
+}
